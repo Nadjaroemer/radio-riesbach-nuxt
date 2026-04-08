1ae72f3d0f3a18e9c24b89bac357f609aa2fb59a
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2026-04-08 14:16:01 +0200

    Add font and brand colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+        }
+      ]
+    }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
