097ad9fe472e05cbb36abdde8af9218d9e413fa8
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2026-04-08 15:27:01 +0200

    i18n module setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,20 +5,25 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  modules: ['@nuxtjs/i18n'],
+
+  i18n: {
+    locales: [
+      { code: 'de', name: 'Deutsch', file: 'de.json' },
+      { code: 'en', name: 'English', file: 'en.json' }
+    ],
+    defaultLocale: 'de',
+    langDir: 'locales/',
+    strategy: 'prefix_except_default'
+  },
+
   css: ['~/assets/css/main.css'],
 
   app: {
     head: {
       link: [
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com',
-          crossorigin: ''
-        },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap'
